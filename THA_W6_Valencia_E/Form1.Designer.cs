b1a3a65a588ff3fc39d5ced222239cd1ec715e88
velcheiana <127375891+velcheiana@users.noreply.github.com>
2023-04-09 23:20:14 +0700

    Add files via upload

---
+++ THA_W6_Valencia_E/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace THA_W6_Valencia_E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Wordle = new System.Windows.Forms.Label();
+            this.lbl_Guess = new System.Windows.Forms.Label();
+            this.textBox_Input = new System.Windows.Forms.TextBox();
+            this.button_Play = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_Wordle
+            // 
+            this.lbl_Wordle.AutoSize = true;
+            this.lbl_Wordle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Wordle.Location = new System.Drawing.Point(68, 28);
+            this.lbl_Wordle.Name = "lbl_Wordle";
+            this.lbl_Wordle.Size = new System.Drawing.Size(98, 24);
+            this.lbl_Wordle.TabIndex = 0;
+            this.lbl_Wordle.Text = "WORDLE";
+            // 
+            // lbl_Guess
+            // 
+            this.lbl_Guess.AutoSize = true;
+            this.lbl_Guess.Location = new System.Drawing.Point(40, 64);
+            this.lbl_Guess.Name = "lbl_Guess";
+            this.lbl_Guess.Size = new System.Drawing.Size(158, 13);
+            this.lbl_Guess.TabIndex = 1;
+            this.lbl_Guess.Text = "Set How Much You Can Guess!";
+            // 
+            // textBox_Input
+            // 
+            this.textBox_Input.Location = new System.Drawing.Point(72, 91);
+            this.textBox_Input.Name = "textBox_Input";
+            this.textBox_Input.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Input.TabIndex = 2;
+            this.textBox_Input.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_Input_KeyPress);
+            // 
+            // button_Play
+            // 
+            this.button_Play.Location = new System.Drawing.Point(82, 127);
+            this.button_Play.Name = "button_Play";
+            this.button_Play.Size = new System.Drawing.Size(75, 23);
+            this.button_Play.TabIndex = 3;
+            this.button_Play.Text = "Play!";
+            this.button_Play.UseVisualStyleBackColor = true;
+            this.button_Play.Click += new System.EventHandler(this.button_Play_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(247, 185);
+            this.Controls.Add(this.button_Play);
+            this.Controls.Add(this.textBox_Input);
+            this.Controls.Add(this.lbl_Guess);
+            this.Controls.Add(this.lbl_Wordle);
+            this.Name = "Form1";
+            this.Text = "Welcome";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Wordle;
+        private System.Windows.Forms.Label lbl_Guess;
+        private System.Windows.Forms.TextBox textBox_Input;
+        private System.Windows.Forms.Button button_Play;
+    }
+}
+
---
+++ THA_W6_Valencia_E/Form2.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
+namespace THA_W6_Valencia_E
+{
+    public partial class Form2 : Form
+    {
+        int counter = 0;
+        int counter2 = 0;
+        List<Button> button1 = new List<Button>();
+        List<Button> button2 = new List<Button>();
+        List<string> alphabet = new List<string>() { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M" };
+        string[] kata = File.ReadAllText("Wordle Word List.txt").Split(',');
+        List<string> daftarkata = new List<string>();
+        string simpan;
+        List<string> tebak = new List<string>();
+        List<string> sama = new List<string>();
+        int ending = Form1.input;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        Form1 form = new Form1();
+        int cek = 0;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            int x = 10;
+            int y = 10;
+            for (int i = 1; i <= Form1.input; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    Button btn = new Button();
+                    btn.Tag = "button" + cek;
+                    btn.Location = new Point(x, y);
+                    btn.Size = new Size(50, 50);
+                    x += 50;
+                    button1.Add(btn);
+                    cek++;
+                }
+                x = 10;
+                y += 50;
+            }
+            foreach (Button a in button1)
+            {
+                this.Controls.Add(a);
+            }
+
+            int samping = 270;
+            int tinggi = 20;
+            foreach (var i in alphabet)
+            {
+                Button button = new Button();
+                button.Text = i.ToString();
+                button.Location = new Point(samping, tinggi);
+                button.Size = new Size(50, 50);
+                this.Controls.Add(button);
+                button.Click += btn2_Click;
+                samping += 60;
+
+                if (i == "P")
+                {
+                    tinggi += 60;
+                    samping = 300;
+                }
+                else if (i == "L")
+                {
+                    tinggi += 60;
+                    samping = 360;
+                }
+            }
+            Button buttondel = new Button();
+            buttondel.Location = new Point(295, 140);
+            buttondel.Size = new Size(55, 40);
+            buttondel.Click += btndel_Click;
+            buttondel.Text = "Delete";
+            this.Controls.Add(buttondel);
+            Button buttonenter = new Button();
+            buttonenter.Location = new Point(780, 140);
+            buttonenter.Size = new Size(55, 40);
+            buttonenter.Click += btnenter_Click;
+            buttonenter.Text = "Enter";
+            this.Controls.Add(buttonenter);
+
+            string guess = kata[new Random().Next(0, kata.Length)];
+            MessageBox.Show(guess);
+            simpan = guess;
+
+            for (int i = 0; i < simpan.Length; i++)
+            {
+                daftarkata.Add(simpan[i].ToString().ToUpper());
+            }
+
+        }
+
+        private void btndel_Click(object sender, EventArgs e)
+        {
+            if (button1[0].Text != "")
+            {
+                button1[cekk - 1].Text = "";
+                cekk--;
+            }
+        }
+
+        private void btnenter_Click(object sender, EventArgs e)
+        {
+            bebas = bebas + 5;
+            int cekk2 = 0;
+            int countjumlahinput = 0;
+           
+            for (int z = counter2; z<counter2 + 5; z++)
+            {
+                if (button1[z].Text == " " || button1[z].Text == "")
+                {
+                    countjumlahinput++;
+                }
+                
+            }
+            if (countjumlahinput > 1)
+            {
+                MessageBox.Show("Please enter a 5 letter word!!", "Invalid Guess", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if(countjumlahinput < 1)
+            {
+                for (int i = counter2; i < counter2 + 5; i++)
+                {
+                    tebak.Add(button1[i].Text);
+                    counter++;
+                }
+                //MessageBox.Show("counter "+counter.ToString());
+                
+                for (int i = counter - 5; i < counter; i++)
+                {
+                    //MessageBox.Show(i.ToString()+"i");
+                    for (int j = counter - 5; j < counter; j++)
+                    {
+                        //MessageBox.Show(j.ToString()+"j");
+                        if (daftarkata[i % 5] == tebak[j])
+                        {
+                            button1[i].BackColor = Color.Yellow;
+                        }
+                    }
+                }
+
+                foreach (var a in daftarkata)
+                {
+                    if (a == tebak[counter2])
+                    {
+                        button1[counter2].BackColor = Color.LightGreen;
+                        cekk2++;
+                    }
+                    counter2++;
+                }
+                if (cekk2 == 5)
+                {
+                    MessageBox.Show("You Win!");
+                    this.Close();
+                }
+                if (counter >= ending * 5)
+                {
+                    MessageBox.Show("You Lose! the answer is " + simpan );
+                    this.Close();
+                }
+            }
+            countjumlahinput = 0;
+            
+        }
+
+        int cekk = 0;
+        int bebas = 4;
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            string karakter = button.Text;
+            for (int i = cekk; i < cekk + 1; i++)
+            {
+                if (cekk < bebas + 1)
+                {
+                    button1[i].Text = karakter;
+                }
+            }
+            cekk++;
+        }
+    }
+}
